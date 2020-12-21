485a96463f6157b392233d3072e0901e9a42c541
Abhinav Rana <ranaabhinav50@gmail.com>
2020-12-21 22:59:20 +0530

    .

---
+++ DecemberCookOff2020/Catheif.java
@@ -0,0 +1,39 @@
+package DecemberCookOff2020;
+import java.util.*;
+public class Catheif {
+	
+	public static void main(String args[]) {
+		try {
+		Scanner sr = new Scanner(System.in);
+		int T = 0;
+		T = sr.nextInt();
+		ArrayList<String> ans = new ArrayList<>();
+		while(T> 0) {
+			T--;
+			int x = sr.nextInt();
+			int y = sr.nextInt();
+			int k = sr.nextInt();
+			int n = sr.nextInt();
+			
+			if((Math.abs(x-y)/k)%2 == 0) {
+				ans.add("YES");
+			}else {
+				ans.add("NO");
+			}
+			
+			
+			
+		}
+		for(String a: ans)
+			System.out.println(a);
+		
+		}
+		
+	catch(Exception e) {
+		
+	}
+	
+	}
+	
+	
+}
