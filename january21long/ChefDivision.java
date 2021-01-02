09cd8510b1f3b51d7267f734b69f3170ac2a3e56
Abhinav Rana <ranaabhinav50@gmail.com>
2021-01-02 21:02:14 +0530

    .

---
+++ january21long/ChefDivision.java
@@ -0,0 +1,60 @@
+package january21long;
+import java.util.*;
+public class ChefDivision {
+	
+	public static void main(String args[]) throws Exception {
+		
+		try {
+		Scanner sr = new Scanner (System.in);
+		
+		int T = sr.nextInt();
+		if(T < 1 || T>1000) {
+			throw new Exception();
+		}
+		ArrayList<Integer> ans = new ArrayList<>();
+		while(T > 0) {
+			T--;
+			int N = sr.nextInt();
+			int K = sr.nextInt();
+			int D = sr.nextInt();
+			
+			if(N < 1 || N > 100 || K <1 || K> Math.pow(10, 9) || D <1 || D> Math.pow(10, 9))
+			{
+				throw new Exception();
+			}
+			
+			
+			int sum = 0;
+			for(int i = 0 ; i< N ; i++) {
+				int A = sr.nextInt();
+				if(N==1 && i == 1) {
+					if(A<1 || A> Math.pow(10, 5));
+				}
+				sum+= A;
+			}
+			int days = (int) Math.floor(sum/K);
+			
+			if(days<= 0) {
+				ans.add(0);
+			}
+			else if(days< D) {
+				ans.add(days);
+			}
+			else if(days> D) {
+				ans.add(D);
+			}
+			
+		}
+		for(int a:ans) {
+			System.out.println(a);
+		}
+		
+		}
+		catch(Exception e) {
+			
+		}
+		
+	}
+	
+	
+}
---
+++ january21long/EncodedString.java
@@ -0,0 +1,38 @@
+package january21long;
+import java.util.*;
+public class EncodedString {
+	
+	public static String decode(String binary) {
+		
+		String ans = "";
+		for(int i = 0 ; i<binary.length() ; i+=4)
+		{
+			ans += (char) (97+ (8*Integer.parseInt(binary.charAt(i)+"")) + (4*Integer.parseInt(binary.charAt(i+1)+"")) + (2*Integer.parseInt(binary.charAt(i+2)+"") ) + (1*Integer.parseInt(binary.charAt(i+3)+"")) );
+//			System.out.println(  (97+(8*Integer.parseInt(binary.charAt(i)+"")) + (4*Integer.parseInt(binary.charAt(i+1)+"")) + (2*Integer.parseInt(binary.charAt(i+2)+"") ) + (1*Integer.parseInt(binary.charAt(i+3)+"")) ));
+		}
+		
+		return ans;
+	}
+	
+	
+	public static void main(String args[]) throws Exception {
+		
+		try {
+			ArrayList<String> str = new ArrayList<>();
+			Scanner sr = new Scanner(System.in);
+			int T = sr.nextInt();
+			while(T> 0) {
+				int N = sr.nextInt();
+				sr.nextLine();
+				str.add( sr.nextLine());
+				T--;
+			}
+			for(String st: str)
+				System.out.println(decode(st));
+		}
+		catch(Exception e) {
+			
+		}
+		
+	}
+}
