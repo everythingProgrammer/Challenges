dee3120ee4e831e1418b2acf99a578b8dee653ea
Abhinav Rana <ranaabhinav50@gmail.com>
2021-01-03 09:34:51 +0530

    .

--- january21long/EncodedString2.java
+++ january21long/EncodedString2.java
@@ -3,7 +3,7 @@ import java.util.*;
  
 public class EncodedString2 {
 	
-	static HashMap<String, String> codes = new HashMap<>();
+	static HashMap<String, Character> codes = new HashMap<>();
 
 	
 	public static String decode(String code) {
@@ -13,11 +13,12 @@ public class EncodedString2 {
 			
 			String bin = code.substring(i, i+4);
 			if(codes.containsKey(bin)) {
+				System.out.println(".");
 				ans += codes.get(bin);
 			}
 			else {
 				char ch = (char) (97+Integer.parseInt( bin ,2));
-				codes.put(bin,  ch+"");
+				codes.put(bin,  ch);
 				ans += ch;
 			}
 			
@@ -28,7 +29,23 @@ public class EncodedString2 {
 	}
 	
 	public static void main(String args[])throws java.lang.Exception {
-		
+		codes.put("0000", 'a');
+		codes.put("0001", 'b');
+		codes.put("0010", 'c');
+		codes.put("0011", 'd');
+		codes.put("0100", 'e');
+		codes.put("0101", 'f');
+		codes.put("0110", 'g');
+		codes.put("0111", 'h');
+		codes.put("1000", 'i');
+		codes.put("1001", 'j');
+		codes.put("1010", 'k');
+		codes.put("1011", 'l');
+		codes.put("1100", 'm');
+		codes.put("1101", 'n');
+		codes.put("1110", 'o');
+		codes.put("1111", 'p');
+
 		try {
 			ArrayList<String> str = new ArrayList<>();
 			Scanner sr = new Scanner(System.in);
---
+++ january21long/EncodedStrings.java
@@ -0,0 +1,28 @@
+package january21long;
+import java.util.*;
+public class EncodedStrings {
+	
+	static HashMap<String , Character> codes= new HashMap<>();
+	
+	
+	
+	public static void main(String args[]) {
+		codes.put("0000", 'a');
+		codes.put("0001", 'b');
+		codes.put("0010", 'c');
+		codes.put("0011", 'd');
+		codes.put("0100", 'e');
+		codes.put("0101", 'f');
+		codes.put("0110", 'g');
+		codes.put("0111", 'h');
+		codes.put("1000", 'i');
+		codes.put("1001", 'j');
+		codes.put("1010", 'k');
+		codes.put("1011", 'l');
+		codes.put("1100", 'm');
+		codes.put("1101", 'n');
+		codes.put("1110", 'o');
+		codes.put("1111", 'p');
+	}
+	
+}
