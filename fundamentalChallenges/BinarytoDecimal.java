769a7ed35eafc5f8de1b98d8b69a407c431a9bcd
Abhinav Rana <ranaabhinav50@gmail.com>
2021-01-25 21:45:32 +0530

    .

---
+++ fundamentalChallenges/BinarytoDecimal.java
@@ -0,0 +1,14 @@
+package fundamentalChallenges;
+import java.util.*;
+public class BinarytoDecimal {
+	
+	public static void main(String args[]) {
+		
+		Scanner sr = new Scanner(System.in);
+		int str = sr.nextInt();
+		
+		System.out.println(Integer.toOctalString(str));
+	}
+	
+	
+}
--- fundamentalChallenges/VonNeumann.java
+++ fundamentalChallenges/VonNeumann.java
@@ -1,5 +1,26 @@
 package fundamentalChallenges;
 import java.util.*;
 public class VonNeumann {
+	
+	public static void main(String args[]) {
+		int t; 
+		Scanner sr = new Scanner(System.in);
+		t = sr.nextInt();
+		
+		String arr[] = new String[t];
+		
+		for(int i = 0 ; i< t; i++) {
+			arr[i] = sr.next();
+		}
+		
+//		System.out.println();
+		for(String st : arr)
+//			System.out.println(st);
+			System.out.println(Integer.parseInt(st,2));
+			
+		}
+		
+		
+	}
+	
 
-}
