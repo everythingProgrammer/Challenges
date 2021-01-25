0396e50dbdcffef9ed0f4246da8eba9c18876489
Abhinav Rana <ranaabhinav50@gmail.com>
2021-01-25 20:00:45 +0530

    Update SimpleInput.java

--- fundamentalChallenges/SimpleInput.java
+++ fundamentalChallenges/SimpleInput.java
@@ -1,5 +1,27 @@
 package fundamentalChallenges;
+import java.util.*;
 
 public class SimpleInput {
-
+	
+	public static void main(String args[]) {
+		int sum = 0;
+		Scanner sr = new Scanner(System.in);
+		ArrayList<Integer> arr = new ArrayList<>();
+		int a = sr.nextInt();
+		sum+=a;
+		
+		while(sum>=0) {
+			arr.add(a);
+			a = sr.nextInt();
+			sum+=a;
+		}
+		
+		for(int b:arr ) {
+			System.out.println(b);
+		}
+		
+		
+	}
+	
+	
 }
