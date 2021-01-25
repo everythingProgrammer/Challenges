1f7999de8d1101a22344200c8695da0adb13a1d4
Abhinav Rana <ranaabhinav50@gmail.com>
2021-01-25 22:24:39 +0530

    Update Armstrong.java

--- fundamentalChallenges/Armstrong.java
+++ fundamentalChallenges/Armstrong.java
@@ -1,5 +1,27 @@
 package fundamentalChallenges;
 import java.util.*;
 public class Armstrong {
-
+	
+	
+	public static void main(String args[]) {
+			Scanner sr = new Scanner(System.in);
+			int c=0,a,temp;  
+		    int n=sr.nextInt();//It is the number to check armstrong  
+		    temp=n;  
+		    while(n>0)  
+		    {  
+		    a=n%10;  
+		    n=n/10;  
+		    c=c+(a*a*a);  
+		    }  
+		    if(temp==c)  
+		    System.out.println("true");   
+		    else  
+		        System.out.println("false");   
+		   }  
+		
+		
+	}
+	
+	
 }
