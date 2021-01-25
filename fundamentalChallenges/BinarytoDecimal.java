954a12098ff908505e514b41f5c45c69cc906c93
Abhinav Rana <ranaabhinav50@gmail.com>
2021-01-25 21:55:32 +0530

    .

--- fundamentalChallenges/BinarytoDecimal.java
+++ fundamentalChallenges/BinarytoDecimal.java
@@ -5,6 +5,11 @@ public class BinarytoDecimal {
 	public static void main(String args[]) {
 		
 		Scanner sr = new Scanner(System.in);
+	
+		// binary to decimal...
+		
+		
+		// Decimal to Octal
 		int str = sr.nextInt();
 		
 		System.out.println(Integer.toOctalString(str));
---
+++ fundamentalChallenges/PrintSeries.java
@@ -0,0 +1,33 @@
+package fundamentalChallenges;
+import java.util.*;
+public class PrintSeries {
+	
+	
+	
+	
+	public static void main(String args[]) {
+		
+		Scanner sr = new Scanner(System.in);
+		
+		int N1 = sr.nextInt();
+		int N2 = sr.nextInt();
+		
+		// N1 terms  3n+2 % N2 != 0
+		int n = 1;
+		
+		int count = 0;
+		while(count< N1) {
+			int sum = 3*n+2;
+			if(sum%N2 != 0) {
+				System.out.println(sum);
+				count++;
+			}
+			n++;
+		}
+		
+		
+		
+	}
+	
+	
+}
