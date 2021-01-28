920d66ad6615b7259b3f29b61fe6c4d5b3f55bed
Abhinav Rana <ranaabhinav50@gmail.com>
2021-01-28 15:38:52 +0530

    .

---
+++ fundamentalChallenges/ManmohanPattern1.java
@@ -0,0 +1,19 @@
+package fundamentalChallenges;
+import java.util.*;
+public class ManmohanPattern1 {
+	
+	
+	
+	public static void main(String args[]) {
+		
+		Scanner sr = new Scanner(System.in);
+		
+		int n = sr.nextInt();
+		
+
+		
+		
+	}
+	
+	
+}
